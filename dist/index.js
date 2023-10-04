4fc4dcb12b540fc504737dd342888bbdacbe79a8
nick <nick.m.dodson@gmail.com>
2023-10-04 12:33:29 -0400

    remove port from express

--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,7 @@ import { createSnapshotProposal } from './utils/index.js';
 import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
-const port = 3000;
+const port = process.env.port;
 const Nouns = new nerman.Nouns(`https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`);
 app.get('/', async (req, res) => {
     res.send('sharkdao-snapbot (:');
